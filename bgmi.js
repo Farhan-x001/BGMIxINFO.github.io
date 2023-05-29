023cd6a56c377e25f30d60d26914e2863809d409
Farhan-x001 <115449922+Farhan-x001@users.noreply.github.com>
2023-05-29 05:54:42 +0530

    my fun website

---
+++ bgmi.js
@@ -0,0 +1,11 @@
+<script>
+    function showMessage() {
+      var message = document.getElementById("message").value;
+      alert("Message: " + message);
+    }
+    
+    function showName() {
+      var name = document.getElementById("name").value;
+      alert("Name: " + name);
+    }
+    </script>
